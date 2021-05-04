67cefaa1c0bb1b64edc748abac131f4228ae8b35
rana2020701 <73128713+rana2020701@users.noreply.github.com>
2021-05-04 02:51:42 -0700

    Update App.js

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ import Expenses from './components/Expenses/Expenses';
 const DUMMY_EXPENSES = [
     {
       id: "e1",
-      title: "Toilet Paper",
+      title: "Radio",
       amount: 94.12,
       date: new Date(2020, 7, 14),
     },
@@ -18,7 +18,7 @@ const DUMMY_EXPENSES = [
     },
     {
       id: "e3",
-      title: "Car Insurance",
+      title: "Book",
       amount: 294.67 ,
       date: new Date(2021, 2, 28),
     },
