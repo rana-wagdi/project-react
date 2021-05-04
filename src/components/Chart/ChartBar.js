cc1ea5cc6cb6fa16ff4f2563ca1ef3125d30dbe1
rana2020701 <73128713+rana2020701@users.noreply.github.com>
2021-05-03 23:37:26 -0700

    add css

--- src/components/Chart/ChartBar.js
+++ src/components/Chart/ChartBar.js
@@ -1,2 +1,22 @@
 import React from 'react';
 
+const ChartBar = props => {
+    let barFillHeight = '0%';
+
+    if(props.max > 0) {
+        barFillHeight = Math.round((props.value / props.maxValue) * 100) + '%';
+    }
+
+    return (
+        <div className='chart-bar'>
+            <div className='chartbar__inner'>
+                <div className='chart-bar__fill' style={{height: barFillHeight}} >
+                    <div className='chart-bar__label'>
+                        {props.label}
+                    </div>
+                </div>
+            </div>
+         </div>
+    )
+}
+export default ChartBar;
